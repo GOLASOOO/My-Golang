3c84f92e780276df697b5879403fd234a780a7c6
Gonzalo Suárez Losada <gonzalo.suarez.losada@gmail.com>
2025-06-26 13:01:02 +0200

    feat: improved Go template

--- main.go
+++ main.go
@@ -2,15 +2,45 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"path"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "¡Hola, Mundo desde Go!")
+	// Parse templates
+	tmpl, err := template.ParseFiles(
+		path.Join("templates", "base.html"),
+		path.Join("templates", "home.html"),
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute template with data
+	data := struct {
+		Title   string
+		Message string
+	}{
+		Title:   "My new website with Go",
+		Message: "Hello World! I'm deploying my new website with Dash!",
+	}
+
+	err = tmpl.ExecuteTemplate(w, "base", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
+	// Serve static files
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Handle routes
 	http.HandleFunc("/", handler)
-	fmt.Println("Servidor iniciado en http://localhost:8080")
+
+	fmt.Println("Server started at http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
